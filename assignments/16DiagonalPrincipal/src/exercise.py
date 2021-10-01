7ad29434c585acd1085f275402bbf41daca416c2
BrendaNievesO <a01252478@itesm.mx>
2021-10-01 17:07:09 +0000

    q

--- assignments/16DiagonalPrincipal/src/exercise.py
+++ assignments/16DiagonalPrincipal/src/exercise.py
@@ -1,6 +1,24 @@
 
 def main():
-  
+    a=int(input(''))
+    b=int(input(''))
+    c=a*b
+    i=0
+    f=0
+    l1=[]
+    l2=[]
+    if a==b:
+        while i<c:
+            i=i+1
+            n=int(input(''))
+            l1.append(n)
+        while f<c:
+            v=l1[f]
+            l2.append(v)
+            f=f+(a+1)
+        print(l2)
+    else:
+        print('La matriz no es cuadrada')
 
 if __name__=='__main__':
     main()
